b923622adbfec1b223d38d92b065e246ca1bb55d
leventejuhasz <92303547+leventejuhasz@users.noreply.github.com>
2021-11-04 11:24:37 +0100

    mappa

--- includes/menu.inc.php
+++ includes/menu.inc.php
@@ -1,15 +1,5 @@
 <?php
 
-$szoveg = "Belépés";
-$link = "belepes.php";
-
-if(!empty($_SESSION["id"])) {
-    $szoveg = $_SESSION["nev"].": Kilépés";
-    $link = "index.php?logout=1";
-} 
-
-$menupontok = array('index' => "Főoldal", 'ulesrend' => "Ülésrend", $link => $szoveg);
-
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,7 +14,7 @@ $menupontok = array('index' => "Főoldal", 'ulesrend' => "Ülésrend", $link => 
             if($_SERVER['REQUEST_URI'] == '/teszt/'.$key) $active = ' active';
             ?>
             <li class="nav-item<?php echo $active; ?>">
-                <a class="nav-link" href="<?php echo $key; ?>"><?php echo $value; ?></a>
+                <a class="nav-link" href="index.php?page=<?php echo $key; ?>"><?php echo $value; ?></a>
             </li>
             <?php            
         }
